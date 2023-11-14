2a55d3399fbfaadf158428cd73e1dadf77107a18
sangramsahane01 <sangram.sahane01@gmail.com>
2023-11-15 00:44:29 +0530

    My Name in My Native Language using unicode.

---
+++ src/selenium_class_practice/CharDemo.java
@@ -0,0 +1,22 @@
+package selenium_class_practice;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		
+		char c1 = '\u0938';
+		char c2 = '\u0902';
+		char c3 = '\u097B';
+		char c4 = '\u093E';
+		char c5 = '\u092E';
+		char c6 = '\u0938';
+		char c7 = '\u0939';
+		char c8 = '\u093E';
+		char c9 = '\u0923';
+		char c10 = '\u0947';
+		
+		
+	System.out.println(c1+""+c2+""+c3+""+c4+""+c5+" "+c6+""+c7+""+c8+""+c9+""+c10);
+	}
+}
+
