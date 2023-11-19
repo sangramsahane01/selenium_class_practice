b9ea100b8cc78ba62ece41fac49617cf39d38647
sangramsahane01 <sangram.sahane01@gmail.com>
2023-11-19 19:20:57 +0530

    2 ways to write infinite for loop

---
+++ src/selenium_class_practice/InfiniteLoop.java
@@ -0,0 +1,22 @@
+package selenium_class_practice;
+
+public class InfiniteLoop {
+
+	public static void main(String[] args) { 
+		
+		 /***Below Is First Code for Infinite Loop***/
+		  
+		   int i=1;
+		    while(i==1) {
+		    	
+		    	System.out.println("Hello");
+		    }
+		    
+		 /***Below Is Second Code for Infinite Loop***/
+		  for(;;) { 
+			  
+			  System.out.println("Hello"); }
+	
+		 
+	  }
+}
